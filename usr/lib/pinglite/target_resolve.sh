66e742729a84ca148732aad6a831bcb608eb9c80
Patrick M. Connolly <patmconnolly@gmail.com>
2025-02-28 11:37:26 -0600

    Inititial test for resolve script.

---
+++ usr/lib/pinglite/target_resolve.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## This checks if the host resolves to an IP.
+## returns 1 if no
+## returns 0 if yes
+
+TARGETHOST=$1
+
+host $TARGETHOST > /dev/null
+if (( $? == 1 )); then
+    exit 1
+else
+    exit 0
+fi
+
+exit 0
