f9d7d106c5e3f89836fd425910fb33e1288c00fc
Patrick Connolly <patmconnolly@gmail.com>
2025-03-04 12:58:20 -0600

    Quotation mark error in function

--- src/function.cpp
+++ src/function.cpp
@@ -1,8 +1,8 @@
 // Library of functions to use as to not clutter up main.
 
-#include 'function.hpp'
-#include 'configuration.hpp'
-#include 'payload.hpp'
+#include "function.hpp"
+#include "configuration.hpp"
+#include "payload.hpp"
 
 // Add function defenitions here.
 
