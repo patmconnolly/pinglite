fe1848e6e808c4a0f9194778a75c4837044dc794
Patrick Connolly <patmconnolly@gmail.com>
2025-03-11 13:23:20 -0500

    Corrected versioning script for clarity and function.

--- versioning.sh
+++ versioning.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <branch_name>"
+  exit 1
+fi
+
 branch="$1"
 year=$(date -u +%Y)
 month=$(date -u +%m)
@@ -7,27 +12,14 @@ day=$(date -u +%d)
 hour=$(date -u +%H)
 minute=$(date -u +%M)
 
+cat <<EOF > include/version.hpp
+// version.h
+//
+// Contains version info passed in immediately before compile time.
+#ifndef VERSION_H
+#define VERSION_H
 COMPILE_DATE=$year-$month-$day
 COMPILE_TIME=$hour:$minute
 COMPILE_BRANCH=$branch
-
-L1="// version.h\n"
-L2="//\n"
-L3="// Contains version info passed in immedietly before compile time.\n"
-L4="#ifndef VERSION_H\n"
-L5="#define VERSION_H\n"
-L6="COMPILE_DATE="+$COMPILE_DATE+"\n"
-L7="COMPILE_TIME="+$COMPILE_TIME+"\n"
-L8="COMPILE_BRANCH="+$COMPILE_BRANCH+"\n"
-L9="#endif //VERSION_H\n"
-
-
-echo $L1 > include/version.hpp
-echo $L2 >> include/version.hpp
-echo $L3 >> include/version.hpp
-echo $L4 >> include/version.hpp
-echo $L5 >> include/version.hpp
-echo $L6 >> include/version.hpp
-echo $L7 >> include/version.hpp
-echo $L8 >> include/version.hpp
-echo $L9 >> include/version.hpp
+#endif //VERSION_H
+EOF
