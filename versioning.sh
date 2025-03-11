ebdb2463cc1d4950a6bb9cf46abe81b2c2a1e693
Patrick Connolly <patmconnolly@gmail.com>
2025-03-11 13:16:13 -0500

    Bad call on compile node.

--- versioning.sh
+++ versioning.sh
@@ -21,6 +21,13 @@ L7="COMPILE_TIME="+$COMPILE_TIME+"\n"
 L8="COMPILE_BRANCH="+$COMPILE_BRANCH+"\n"
 L9="#endif //VERSION_H\n"
 
-for i in {1..9}; do
-	echo "${L$i}" >> include/version.hpp
-done
+
+echo $L1 > include/version.hpp
+echo $L2 > include/version.hpp
+echo $L3 > include/version.hpp
+echo $L4 > include/version.hpp
+echo $L5 > include/version.hpp
+echo $L6 > include/version.hpp
+echo $L7 > include/version.hpp
+echo $L8 > include/version.hpp
+echo $L9 > include/version.hpp
