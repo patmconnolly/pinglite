3ae9e86193ddb311a6374a72f8de4d0bfdefce71
Patrick Connolly <patmconnolly@gmail.com>
2025-03-17 13:43:43 -0500

    Passing file into config update call.

--- src/function.cpp
+++ src/function.cpp
@@ -41,7 +41,7 @@ namespace function {
 			do {
 				(*i)++;
 				std::cout << "Beginning parse of configuration file: " << argv[*i] << "." << std::endl;
-				result = conf->update();
+				result = conf->update(argv[*i]);
 			} while (*i < argc - 1 and result == 0);
 		}
 		else {
