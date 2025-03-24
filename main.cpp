780c0fa795d58d22eff2626854fdae94a19b6b28
Patrick Connolly <patmconnolly@gmail.com>
2025-03-24 13:04:27 -0500

    Incomplete string call.

--- main.cpp
+++ main.cpp
@@ -163,9 +163,9 @@ int main(int argc, char* argv[]) {
 					if (reportRequired) { 
 						reportText += "Host ----------------: " + conf->getHOST() + "\n";
 						reportText += Data->alertText();
-						reportText += "Return Code ---------: " + std::str(conf->getRETURNCODE()) + "\n";
-						reportText += "Certificate Valid ---: " + std::str(conf->getSSLVALID()) + "\n";
-						reportText += "Certificate Expires in " + std::str(conf->getSSLEXPIRYREMINDER()) + " days.\n";
+						reportText += "Return Code ---------: " + std::string(conf->getRETURNCODE()) + "\n";
+						reportText += "Certificate Valid ---: " + std::string(conf->getSSLVALID()) + "\n";
+						reportText += "Certificate Expires in " + std::string(conf->getSSLEXPIRYREMINDER()) + " days.\n";
 					}
 					Data->write();
 					delete Data;
