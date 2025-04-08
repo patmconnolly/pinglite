47b0f8d5976cfbae15c78682cc21f60dcd411a27
Patrick Connolly <patmconnolly@gmail.com>
2025-04-08 11:39:45 -0500

    Removing full license from packaged binary.

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ hour=$(date -u +%H)
 minute=$(date -u +%M)
 
 #Add each filename to be included in final package, separated by a space.
-IncludedFilenames="$basefilename LICENSE"
+IncludedFilenames="$basefilename"
 
 if [[ "$branch" == "dev" ]]; then
 	outputfile="$basefilename-$branch-$year.$month.$day-$hour.$minute.tar"
