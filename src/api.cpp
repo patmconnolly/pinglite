80e11c09f1ef496a29299f12ec41722689263802
Patrick Connolly <patmconnolly@gmail.com>
2025-04-09 11:56:00 -0500

    Remove tracers.

--- src/api.cpp
+++ src/api.cpp
@@ -35,9 +35,6 @@ api::~api() {}
 
 void api::trigger() {
     // Set CURL to Trigger Payload Here
-    function::debug("URL: ", this->URL);
-    function::debug("HEADERS: ", this->HEADERS);
-    function::debug("PAYLOAD: ", this->PAYLOAD);
     auto handle = curl_easy_init();
     struct curl_slist* headers = nullptr;
 
